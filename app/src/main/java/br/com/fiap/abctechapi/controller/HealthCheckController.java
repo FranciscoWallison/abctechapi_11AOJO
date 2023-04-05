49721cc65ac27a0acbcccdc4d3b438da6187c7b5
francisco wallison <franciscowallison@gmail.com>
2023-04-05 18:35:28 -0300

    first commit

---
+++ app/src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -0,0 +1,34 @@
+package br.com.fiap.abctechapi.controller;
+
+import br.com.fiap.abctechapi.util.VersionComponent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+
+    private final VersionComponent versionComponent;
+
+    public HealthCheckController(@Autowired VersionComponent versionComponent){
+        this.versionComponent = versionComponent;
+    }
+
+    @GetMapping
+    public ResponseEntity<String> status(){
+        return ResponseEntity.ok("UP");
+    }
+
+    @GetMapping("version")
+    public ResponseEntity<String> version(){
+        return ResponseEntity.ok(this.versionComponent.getVersion());
+    }
+
+}
